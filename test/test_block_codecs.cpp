4994d2465e06d5b975362976edeb81e1139fca1a
Agustín González <agustin.ang.92@gmail.com>
2020-02-24 01:05:07 -0300

    Fix unit tests of block encoding

--- test/test_block_codecs.cpp
+++ test/test_block_codecs.cpp
@@ -1,25 +1,25 @@
 #define CATCH_CONFIG_MAIN
 #include "catch2/catch.hpp"
 
-#include <vector>
 #include <cstdlib>
+#include <vector>
 
 #include "codec/block_codecs.hpp"
 #include "codec/maskedvbyte.hpp"
-#include "codec/streamvbyte.hpp"
 #include "codec/qmx.hpp"
-#include "codec/varintgb.hpp"
-#include "codec/simple8b.hpp"
 #include "codec/simdbp.hpp"
 #include "codec/simple16.hpp"
+#include "codec/simple8b.hpp"
+#include "codec/streamvbyte.hpp"
+#include "codec/varintgb.hpp"
 
 #include "test_common.hpp"
 
 template <typename BlockCodec>
-void test_block_codec()
+void test_block_codec(std::vector<size_t> sizes = {
+                          1, 16, BlockCodec::block_size - 1, BlockCodec::block_size})
 {
-    std::vector<size_t> sizes = {1, 16, BlockCodec::block_size - 1, BlockCodec::block_size};
-    for (auto size: sizes) {
+    for (auto size : sizes) {
         std::vector<uint32_t> values(size);
         std::generate(values.begin(), values.end(), []() { return (uint32_t)rand() % (1 << 12); });
 
@@ -31,11 +31,9 @@ void test_block_codec()
             }
             std::vector<uint8_t> encoded;
             BlockCodec::encode(values.data(), sum_of_values, values.size(), encoded);
-
             std::vector<uint32_t> decoded(values.size());
-            uint8_t const* out = BlockCodec::decode(encoded.data(), decoded.data(),
-                                                    sum_of_values, values.size());
-
+            uint8_t const *out =
+                BlockCodec::decode(encoded.data(), decoded.data(), sum_of_values, values.size());
             REQUIRE(encoded.size() == out - encoded.data());
             REQUIRE(std::equal(values.begin(), values.end(), decoded.begin()));
         }
@@ -44,14 +42,19 @@ void test_block_codec()
 
 TEST_CASE("block_codecs")
 {
-    test_block_codec<pisa::optpfor_block>();
-    test_block_codec<pisa::varint_G8IU_block>();
     test_block_codec<pisa::streamvbyte_block>();
     test_block_codec<pisa::maskedvbyte_block>();
     test_block_codec<pisa::interpolative_block>();
     test_block_codec<pisa::qmx_block>();
     test_block_codec<pisa::varintgb_block>();
     test_block_codec<pisa::simple8b_block>();
-    test_block_codec<pisa::simdbp_block>();
     test_block_codec<pisa::simple16_block>();
+
+    // SIMDBP and PFD implementations works with chunks equals to block size.
+    test_block_codec<pisa::simdbp_block>({pisa::simdbp_block::block_size});
+    test_block_codec<pisa::optpfor_block>({pisa::optpfor_block::block_size});
+
+    // The minimum list size required for varintG8IU implementation is 8.
+    test_block_codec<pisa::varint_G8IU_block>(
+        {8, 16, pisa::varint_G8IU_block::block_size - 1, pisa::varint_G8IU_block::block_size});
 }
--- test/test_block_freq_index.cpp
+++ test/test_block_freq_index.cpp
@@ -5,24 +5,25 @@
 
 #include "codec/block_codecs.hpp"
 #include "codec/maskedvbyte.hpp"
-#include "codec/streamvbyte.hpp"
 #include "codec/qmx.hpp"
-#include "codec/varintgb.hpp"
-#include "codec/simple8b.hpp"
-#include "codec/simple16.hpp"
 #include "codec/simdbp.hpp"
+#include "codec/simple16.hpp"
+#include "codec/simple8b.hpp"
+#include "codec/streamvbyte.hpp"
+#include "codec/varintgb.hpp"
 #include "temporary_directory.hpp"
 
 #include "block_freq_index.hpp"
 #include "mappable/mapper.hpp"
 #include "mio/mmap.hpp"
 
-#include <vector>
-#include <cstdlib>
 #include <algorithm>
+#include <cstdlib>
+#include <type_traits>
+#include <vector>
 
 template <typename BlockCodec>
-void test_block_freq_index()
+void test_block_freq_index(bool adjust_to_block_size = false)
 {
     pisa::global_parameters params;
     uint64_t universe = 20000;
@@ -31,17 +32,21 @@ void test_block_freq_index()
 
     typedef std::vector<uint64_t> vec_type;
     std::vector<std::pair<vec_type, vec_type>> posting_lists(30);
-    for (auto& plist: posting_lists) {
+    for (auto &plist : posting_lists) {
         double avg_gap = 1.1 + double(rand()) / RAND_MAX * 10;
-        uint64_t n = uint64_t(universe / avg_gap);
+
+        uint64_t n = universe / avg_gap;
+        if (adjust_to_block_size) {
+            // Removes last chunk if its size is lesser than the encoder block size.
+            n -= n % BlockCodec::block_size;
+        }
+
         plist.first = random_sequence(universe, n, true);
         plist.second.resize(n);
-        std::generate(plist.second.begin(), plist.second.end(),
-                      []() { return (rand() % 256) + 1; });
+        std::generate(
+            plist.second.begin(), plist.second.end(), []() { return (rand() % 256) + 1; });
 
-        b.add_posting_list(n, plist.first.begin(),
-                           plist.second.begin(), 0);
-
+        b.add_posting_list(n, plist.first.begin(), plist.second.begin(), 0);
     }
 
     Temporary_Directory tmpdir;
@@ -58,14 +63,12 @@ void test_block_freq_index()
         pisa::mapper::map(coll, m);
 
         for (size_t i = 0; i < posting_lists.size(); ++i) {
-            auto const& plist = posting_lists[i];
+            auto const &plist = posting_lists[i];
             auto doc_enum = coll[i];
             REQUIRE(plist.first.size() == doc_enum.size());
             for (size_t p = 0; p < plist.first.size(); ++p, doc_enum.next()) {
-                MY_REQUIRE_EQUAL(plist.first[p], doc_enum.docid(),
-                                 "i = " << i << " p = " << p);
-                MY_REQUIRE_EQUAL(plist.second[p], doc_enum.freq(),
-                                 "i = " << i << " p = " << p);
+                MY_REQUIRE_EQUAL(plist.first[p], doc_enum.docid(), "i = " << i << " p = " << p);
+                MY_REQUIRE_EQUAL(plist.second[p], doc_enum.freq(), "i = " << i << " p = " << p);
             }
             REQUIRE(coll.num_docs() == doc_enum.docid());
         }
@@ -74,7 +77,6 @@ void test_block_freq_index()
 
 TEST_CASE("block_freq_index")
 {
-    test_block_freq_index<pisa::optpfor_block>();
     test_block_freq_index<pisa::varint_G8IU_block>();
     test_block_freq_index<pisa::streamvbyte_block>();
     test_block_freq_index<pisa::maskedvbyte_block>();
@@ -83,5 +85,8 @@ TEST_CASE("block_freq_index")
     test_block_freq_index<pisa::qmx_block>();
     test_block_freq_index<pisa::simple8b_block>();
     test_block_freq_index<pisa::simple16_block>();
-    test_block_freq_index<pisa::simdbp_block>();
+
+    // SIMDBP and PFD implementations works with chunks equals to block size.
+    test_block_freq_index<pisa::simdbp_block>(pisa::simdbp_block::block_size);
+    test_block_freq_index<pisa::optpfor_block>(pisa::simdbp_block::block_size);
 }
