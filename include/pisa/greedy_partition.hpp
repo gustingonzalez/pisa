b57ecee58ed5ab0585fa04a13b0166bb82796b3b
Agustín González <agustin.ang.92@gmail.com>
2020-05-01 01:42:32 -0300

    Add greedy partitioning algorithm

---
+++ include/pisa/greedy_partition.hpp
@@ -0,0 +1,60 @@
+#pragma once
+
+namespace pisa {
+    struct GreedyPartition {
+        template <typename Iterator>
+        static std::vector<uint32_t> compute(Iterator list, uint32_t n) {
+            std::vector<uint32_t> partitions;
+            uint32_t last_computed(-1);
+            uint32_t list_size = 0;
+            int step = 8;
+
+            // start-end window
+            uint32_t start = 0;
+            uint32_t end = step < n ? step - 1 : n - 1;
+            uint32_t block_size = end - start + 1;
+            uint32_t weight = list[end] / block_size;
+            bool increment_window = false;
+
+            uint32_t end_candidate;
+            uint32_t block_size_cantidate;
+            uint32_t weight_candidate;
+            do {
+                if (increment_window) {
+                    // Updates end, weight and block size based on candidate.
+                    end = end_candidate;
+                    weight = weight_candidate;
+                    block_size = block_size_cantidate;
+                    increment_window = false;
+                }
+
+                end_candidate = (end + step) < n ? (end + step) : n - 1;
+                block_size_cantidate = (end_candidate - start) + 1;
+                weight_candidate = list[end_candidate] / block_size_cantidate;
+
+                // If the previously computed weight is lesser than the new candidate...
+                if (weight_candidate > weight) {
+                    // Appends block size to partitions.
+                    partitions.push_back(block_size);
+
+                    // Reinitializes a new window.
+                    start = end + 1;
+                    end = start + step < n ? start + (step - 1) : n - 1;
+                    block_size = (end - start) + 1;
+                    weight = list[end] / block_size;
+                } else {
+                    increment_window = true;
+                }
+
+                // Break condition.
+                if (end_candidate == end && end == n - 1) {
+                    partitions.push_back(block_size);
+                    break;
+                }
+
+            } while(true);
+
+            return partitions;
+        }
+    };
+}
