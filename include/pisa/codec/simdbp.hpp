c88c77073ba3a17e9145d13ac7b875d16e109aee
Agustín González <agustin.ang.92@gmail.com>
2020-05-25 16:39:36 -0300

    Update simdbp encoder to work with chunks of 256 items

--- include/pisa/codec/simdbp.hpp
+++ include/pisa/codec/simdbp.hpp
@@ -10,29 +10,61 @@ extern "C" {
 
 namespace pisa {
 struct simdbp_block {
-    static const uint64_t block_size = 256;
-    static void encode(uint32_t const *in,
-                       uint32_t sum_of_values,
-                       size_t n,
-                       std::vector<uint8_t> &out) {
+   static const uint64_t block_size = 256;
+ 
+   static void encode_128(uint32_t const *in, std::vector<uint8_t> &out, size_t n, uint32_t b) {
+       thread_local std::vector<uint8_t> buf(8*n);
+       uint8_t * buf_ptr = buf.data();
+       simdpackwithoutmask(in, (__m128i *)buf_ptr, b);
+       out.insert(out.end(), buf.data(), buf.data() + b * sizeof(__m128i));
+   }
+ 
+   static void encode(uint32_t const *in,
+                      uint32_t sum_of_values,
+                      size_t n,
+                      std::vector<uint8_t> &out) {
+       assert(n <= block_size);
+       uint32_t *src = const_cast<uint32_t *>(in);
+       // It's required to set the SIMDBlockSize to 256!
+       assert(SIMDBlockSize == 256);
 
-        assert(n <= block_size);
-        uint32_t *src = const_cast<uint32_t *>(in);
-        uint32_t b = maxbits(in);
-        thread_local std::vector<uint8_t> buf(8*n);
-        uint8_t * buf_ptr = buf.data();
-        *buf_ptr++ = b;
-        simdpackwithoutmask(src, (__m128i *)buf_ptr, b);
-        out.insert(out.end(), buf.data(), buf.data() + b * sizeof(__m128i) + 1);
-    }
-    static uint8_t const *decode(uint8_t const *in,
-                                 uint32_t *out,
-                                 uint32_t sum_of_values,
-                                 size_t n) {
-        assert(n <= block_size);
-        uint32_t b = *in++;
-        simdunpack((const __m128i *)in, out, b);
-        return in +  b * sizeof(__m128i);
-    }
+       // Computes and encodes the bits required per element.
+       uint8_t b = maxbits(in);
+       out.push_back(b);
+
+       // Encodes the first 128-items.
+       encode_128(in, out, n, b);
+       // Encodes the remaining 128-items.
+       encode_128(in+=128, out, n, b);
+   }
+ 
+    static uint8_t const *decode_128(uint8_t const *in,
+                                uint32_t *out,
+                                uint32_t b)
+   {
+       simdunpack((const __m128i *)in, out, b);
+       return in +  b * sizeof(__m128i);
+   }
+ 
+   static uint8_t const *decode(uint8_t const *in,
+                                uint32_t *out,
+                                uint32_t sum_of_values,
+                                size_t n) {
+       assert(n <= block_size);
+       uint32_t b = *in++;
+ 
+       // Reads the first 128 items (SIMBP works with 128-chunks).
+       simdunpack((const __m128i *)in, out, b);
+ 
+       // Given that in the previous one instruction are readed the first 128 items,
+       // and being 'b' the bits used per item, it's required to move 'b' bytes to
+       // read the remaining 128. Then b = 128b/8 => b = 16b. Also, note that the
+       // next two instructions are independents of the previous one, avoiding 'data
+       // hazard' on pipelining.
+       uint8_t const * second_unpack = in + 16*b;
+       simdunpack((const __m128i *)second_unpack, out+=128, b);
+ 
+       return second_unpack + 16*b;
+   }
 };
 } // namespace pisa
