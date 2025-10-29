17776ab4ee2608825b8256f0071e8cd2ed449beb
Agustín González <agustin.ang.92@gmail.com>
2025-10-29 19:22:08 -0300

    Add bit-packing codec implementation

--- include/pisa/multicompression/block_codecs.hpp
+++ include/pisa/multicompression/block_codecs.hpp
@@ -11,6 +11,135 @@
 #include "codec/simple16.hpp"
 
 namespace pisa {
+struct BitPackingBlockCodec {
+
+    // Computes how many bits are needed to store an specific value.
+    static uint32_t bit_width(uint32_t value) {
+        return value == 0 ? 0U : static_cast<uint32_t>(32U - __builtin_clz(value));
+    }
+
+    // Computes the maximum bit width required by each value of a block.
+    static uint32_t max_bits(std::uint32_t const* in, std::size_t n) {
+        if (n == 0) {
+            return 0;
+        }
+        auto const* max = std::max_element(in, in + n);
+        return bit_width(*max);
+    }
+
+    // Computes the payload size in bytes.
+    static std::size_t compute_payload_size(std::uint32_t b, std::size_t n) {
+        if (b == 0 || n == 0) {
+            return 0;
+        }
+        return (static_cast<std::size_t>(b) * n + 7) / 8;
+    }
+
+    // Computes the total bytes required, including the (one-byte) header.
+    static std::size_t compute_encoded_size(std::uint32_t b, std::size_t n) {
+        return 1 + compute_payload_size(b, n);
+    }
+
+    static std::size_t compute_encoded_size(std::uint32_t const* in, std::size_t n) {
+        return compute_encoded_size(max_bits(in, n), n);
+    }
+
+    static void encode(
+        std::uint32_t const* in,
+        std::uint32_t,
+        std::size_t n,
+        std::vector<std::uint8_t>& out
+    ) {
+        uint32_t b = max_bits(in, n);
+
+        std::size_t encoded_size = compute_encoded_size(b, n);
+        auto start = out.size();
+        out.resize(start + encoded_size);
+        auto* header = out.data() + start;
+        auto* data = header + 1;
+
+        // Add `b` as one-byte header. If it is 0, then all values are zero; so
+        // no payload bytes is needed.
+        *header = static_cast<uint8_t>(b);
+        if (b == 0) {
+            return;
+        }
+
+        // Note that payload is written in little-endian bytes. Advantages:
+        // - Values are appended with a masking and shift; then simple OR.
+        // - Decoding implementation just 'mirrors' encoding.
+        //
+        // Example with 'in = [5, 2, 7, 1, 3]':
+        // Human-readable, big-endian => 1010101 11001011 => 101 010 111 001 011
+        // Encoder output, little-endian (by byte) => 11010101 00110011 => 101 010 111 100 110
+
+        // Bit mask to isolate one packed value. For b=32 avoids shifting (UB).
+        uint32_t mask = b == 32 ? 0xFFFFFFFFU : static_cast<uint32_t>((uint64_t(1) << b) - 1);
+
+        // Buffer that accumulates read bits.
+        uint64_t buffer = 0;
+
+        // Count of valid bits currently stored in `buffer`.
+        unsigned bits_in_buffer = 0;
+
+        // Number of bytes already written to `data`.
+        size_t written = 0;
+
+        for (size_t i = 0; i < n; ++i) {
+            buffer |= (uint64_t(in[i]) & mask) << bits_in_buffer;
+            bits_in_buffer += b;
+
+            // Flush the buffer when holds at least one full byte (8 bits), the
+            // size of each element of `out`.
+            while (bits_in_buffer >= 8) {
+                // Write the lowest 8 bits.
+                data[written++] = static_cast<uint8_t>(buffer & 0xFF);
+                // Remove emitted bits.
+                buffer >>= 8;
+                // Adjust bit counter.
+                bits_in_buffer -= 8;
+            }
+        }
+
+        // Emit any remaining bits left in the buffer.
+        if (bits_in_buffer != 0) {
+            data[written++] = static_cast<uint8_t>(buffer & 0xFF);
+        }
+        // Zero any trailing padding bytes we might have reserved.
+        std::fill(data + written, data + compute_payload_size(b, n), 0);
+    }
+
+    static uint8_t const* decode(
+        uint8_t const* in,
+        uint32_t* out,
+        uint32_t,
+        std::size_t n
+    ) {
+        uint32_t b = *in++;
+        if (b == 0) {
+            std::fill_n(out, n, uint32_t(0));
+            return in;
+        }
+
+        uint32_t mask = b == 32 ? 0xFFFFFFFFU : static_cast<uint32_t>((uint64_t(1) << b) - 1);
+        auto const* data = in;
+        uint64_t buffer = 0;
+        unsigned bits_in_buffer = 0;
+
+        // Mirror of `encode` function.
+        for (std::size_t i = 0; i < n; ++i) {
+            while (bits_in_buffer < b) {
+                buffer |= uint64_t(*data++) << bits_in_buffer;
+                bits_in_buffer += 8;
+            }
+            out[i] = static_cast<uint32_t>(buffer & mask);
+            buffer >>= b;
+            bits_in_buffer -= b;
+        }
+        return data;
+    }
+};
+
 struct all_ones_block {
     static constexpr std::uint64_t block_size = 128;
 
